7464b74cad483badfef5a4eed35c436f44a1757d
HyeKyungAhn <hai1021@hanmail.net>
2023-06-04 07:40:59 +0900

    feat: Add feature for random product draw

--- src/pachinko/ProductContainer.java
+++ src/pachinko/ProductContainer.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -47,5 +48,33 @@ public class ProductContainer {
 
     private void setProductList(Set<Product> productList){
         this.productList = productList;
+    }
+
+    public Product getRandomPrizeByGrade(String grade, LocalDateTime dateTime) {
+        List<Product> availablePrizes = getAvailableProductsByGrade(grade, dateTime);
+        if (availablePrizes.isEmpty()) {
+            return null;
+        }
+
+        int randomIndex = (int) (Math.random() * availablePrizes.size());
+
+        return availablePrizes.get(randomIndex);
+    }
+
+    private List<Product> getAvailableProductsByGrade(String grade, LocalDateTime dateTime) {
+        List<Product> avaliableProductList = new ArrayList<>();
+        int compareResult;
+
+        for(Product p : productList){
+            compareResult = dateTime.compareTo(LocalDateTime.parse(p.getExpirationDate()));
+
+            if (p.getGrade().equals("A") && compareResult <= 0) { // 0(equal), -1(before expirationDate)
+                avaliableProductList.add(p);
+                continue;
+            }
+            avaliableProductList.add(p);
+        }
+
+        return avaliableProductList;
     }
 }
