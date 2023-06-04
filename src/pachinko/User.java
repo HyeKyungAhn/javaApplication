fdfa69028dcb1f87e0403acc63a78412f93d2b8d
HyeKyungAhn <hai1021@hanmail.net>
2023-06-04 22:47:13 +0900

    feat: Add user having wallet

---
+++ src/pachinko/User.java
@@ -0,0 +1,30 @@
+package pachinko;
+
+import java.util.List;
+
+public class User {
+    private VirtualWallet wallet;
+    private int bGradeDrawCount;
+
+    User(){}
+
+    User(VirtualWallet wallet){
+        this.wallet = wallet;
+    }
+
+    public VirtualWallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(VirtualWallet wallet) {
+        this.wallet = wallet;
+    }
+
+    public int getbGradeDrawCount() {
+        return bGradeDrawCount;
+    }
+
+    public void setbGradeDrawCount(int bGradeDrawCount) {
+        this.bGradeDrawCount = bGradeDrawCount;
+    }
+}
