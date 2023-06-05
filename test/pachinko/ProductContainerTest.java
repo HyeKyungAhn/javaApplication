9b50ee32cbde5a9b6e6e66d49ad68a9f4112afef
HyeKyungAhn <hai1021@hanmail.net>
2023-06-05 18:52:42 +0900

    test: Add test for grade kind

--- test/pachinko/ProductContainerTest.java
+++ test/pachinko/ProductContainerTest.java
@@ -16,14 +16,33 @@ public class ProductContainerTest {
         productContainer = new ProductContainer();
     }
 
-    //총 10종류의 상품이 제공되는가?
+    //총 10종류의 다른 상품이 제공되는가?
     @Test
-    public void testProvidedProductKindNum(){
-        Set<String> types1 = productContainer.getProductList().stream()
+    public void testProvidedProductDistinctKind(){
+        Set<String> types = productContainer.getProductList().stream()
                 .map(Product::getType)
                 .collect(Collectors.toSet());
 
-        assertEquals(10, types1.size());
+        assertEquals(10, types.size());
     }
 
+    //A,B 등급의 상품이 최소 2종류 이상인가?
+    @Test
+    public void testMoreThanTwoProvidedProductKindsPerGrade(){
+        Set<Product> products = productContainer.getProductList();
+        int aGradeCount = 0;
+        int bGradeCount = 0;
+
+        for(Product p : products){
+            if (p.getGrade().equals("A")) {
+                aGradeCount++;
+            } else if (p.getGrade().equals("B")) {
+                bGradeCount++;
+            }
+
+        }
+
+        assertTrue(aGradeCount >= 2);
+        assertTrue(bGradeCount >= 2);
+    }
 }
