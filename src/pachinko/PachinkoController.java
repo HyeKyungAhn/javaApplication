23b1ae7c59978fd6426695725563d52d89da7ae6
HyeKyungAhn <hai1021@hanmail.net>
2023-06-05 20:37:39 +0900

    feat: Create pahinko controller

---
+++ src/pachinko/PachinkoController.java
@@ -0,0 +1,20 @@
+package pachinko;
+
+import java.util.Map;
+
+public class PachinkoController implements Controller{
+    @Override
+    public void main() {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public Map<String, Object> executeCommand(String[] dividedInput) {
+        return null;
+    }
+}
