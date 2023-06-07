0ce8ba63cbda97f2efab41205c0fca68aa9ef47d
HyeKyungAhn <hai1021@hanmail.net>
2023-06-08 02:54:49 +0900

    feat: Add container for managing department

---
+++ src/main/java/deptHeadCounter/DepartmentContainer.java
@@ -0,0 +1,48 @@
+package deptHeadCounter;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DepartmentContainer {
+    private Department root;
+
+    DepartmentContainer(){
+        root = new Department("*", 0);
+        root.addChild("unassigned", 0);
+    }
+
+    public Department getRoot(){
+        return root;
+    }
+
+    public Department getDeptIfExist(String deptName) {
+        Queue<Department> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            Department currentDept = queue.poll();
+            if (currentDept.getName().equals(deptName)) {
+                return currentDept;
+            }
+
+            queue.addAll(currentDept.getChildren());
+        }
+
+        return null;
+    }
+
+    public int getTotalHeadCount(Department highestDept){
+        Queue<Department> queue = new LinkedList<>();
+        int totalCount = 0;
+        queue.offer(highestDept);
+
+        while (!queue.isEmpty()) {
+            Department currentDept = queue.poll();
+
+            totalCount += currentDept.getCount();
+            queue.addAll(currentDept.getChildren());
+        }
+
+        return totalCount;
+    }
+}
