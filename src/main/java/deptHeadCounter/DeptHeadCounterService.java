c1166a771ebb3c757381548c716aeb7d92e5b0dc
HyeKyungAhn <hai1021@hanmail.net>
2023-06-09 19:05:41 +0900

    fix: Change return type for more specific errormsg

--- src/main/java/deptHeadCounter/DeptHeadCounterService.java
+++ src/main/java/deptHeadCounter/DeptHeadCounterService.java
@@ -6,28 +6,34 @@ import java.util.Map;
 public class DeptHeadCounterService {
     public static final String NODE_NAME_STORE_UNASSIGNED_DEPT = "unassigned";
 
-    public boolean saveDeptPersonnelInfo(DepartmentContainer container, String[] input){
+    public Map<String, Object> saveDeptPersonnelInfo(DepartmentContainer container, String[] input){
+        Map<String, Object> saveResult = new HashMap<>();
         String deptName;
-        int count = 0;
+        int count;
 
         try {
             deptName = input[0];
             count = Integer.parseInt(input[2]);
         } catch (NumberFormatException | NullPointerException e) {
-            return false;
+            saveResult.put("saveResult", DHCOutputStatus.SAVE_PERSONNEL_INFO_FAIL);
+            return saveResult;
         }
 
         Department savedDeptInfo = container.getDeptIfExist(deptName);
 
         if(savedDeptInfo != null){
-            return savedDeptInfo.setCount(count);
+            saveResult.put("saveResult", getResultStatus(savedDeptInfo.setCount(count)));
+            return saveResult;
         }
 
         Department dept = container.getDeptIfExist(NODE_NAME_STORE_UNASSIGNED_DEPT);
-        return dept.addChild(deptName, count);
+        saveResult.put("saveResult", getResultStatus(dept.addChild(deptName, count)));
+
+        return saveResult;
     }
 
-    public DHCServiceStatus updateDeptStructure(DepartmentContainer container, String[] inputs){
+    public Map<String, Object> updateDeptStructure(DepartmentContainer container, String[] inputs){
+        Map<String, Object> updateResult = new HashMap<>();
         String operation = inputs[1];
         String parentName = operation.equals("<") ? inputs[2] : inputs[0];
         String childName = operation.equals("<") ? inputs[0] : inputs[2];
@@ -36,23 +42,27 @@ public class DeptHeadCounterService {
         Department childDept = container.getDeptIfExist(childName);
 
         if (parentDept == null || childDept == null) {
-            return DHCServiceStatus.FAIL_DEPT_NOT_EXIST; //먼저 부서 입력해달라 부서 정보가 없다.
+            updateResult.put("updateResult", DHCOutputStatus.UPDATE_FAIL_DEPT_NOT_EXIST);
+            return updateResult;
         }
 
         if (!parentDept.getName().equals("*")
                 && (parentDept.getParent() == null || isUnassignedDept(parentDept.getParent()))) {
-            return DHCServiceStatus.FAIL_PARENT_NOT_ASSIGNED; //상위부서가 트리구조 내에 있어야한다.
+            updateResult.put("updateResult", DHCOutputStatus.UPDATE_FAIL_PARENT_NOT_ASSIGNED);
+            return updateResult;
         }
 
 
         if (childDept.getParent() != null && !isUnassignedDept(childDept.getParent())) {
-            return DHCServiceStatus.FAIL_ALREADY_HAVE_PARENT; //이미 상위부서가 있다.
+            updateResult.put("updateResult", DHCOutputStatus.UPDATE_FAIL_ALREADY_HAVE_PARENT);
+            return updateResult;
         }
 
         childDept.getParent().getChildren().remove(childDept); //unassiged에서 자기자신 지우기
         parentDept.addChild(childDept);
+        updateResult.put("updateResult", DHCOutputStatus.UPDATE_DEPT_COMPOSITION_SUCCESS);
 
-        return DHCServiceStatus.SAVE_DEPT_COMPOSITION_SUCCESS;
+        return updateResult;
     }
 
     public Map<String, Integer> calculateTopLevelDeptPersonnelSum(DepartmentContainer container){
@@ -68,5 +78,13 @@ public class DeptHeadCounterService {
 
     private boolean isUnassignedDept(Department dept) {
         return dept.getName().equals(NODE_NAME_STORE_UNASSIGNED_DEPT);
+    }
+
+    private DHCOutputStatus getResultStatus(boolean isSuccess) {
+        if (isSuccess) {
+            return DHCOutputStatus.SAVE_PERSONNEL_INFO_SUCCESS;
+        } else {
+            return DHCOutputStatus.SAVE_PERSONNEL_INFO_FAIL;
+        }
     }
 }
