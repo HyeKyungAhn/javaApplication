ab91d953b6e072a8962f9173f4309d325adb637f
HyeKyungAhn <hai1021@hanmail.net>
2023-06-09 19:53:56 +0900

    fix: Add return for CANNOT_ADD_ITSELF_AS_CHILD

--- src/main/java/deptHeadCounter/DeptHeadCounterService.java
+++ src/main/java/deptHeadCounter/DeptHeadCounterService.java
@@ -54,8 +54,9 @@ public class DeptHeadCounterService {
 
 
         if (childDept.getParent() != null && !isUnassignedDept(childDept.getParent())) {
-            if (childDept.getParent().getName().equals(childName)) {
+            if (parentName.equals(childName)) {
                 updateResult.put("updateResult", DHCOutputStatus.UPDATE_FAIL_CANNOT_ADD_ITSELF_AS_CHILD);
+                return updateResult;
             }
             updateResult.put("updateResult", DHCOutputStatus.UPDATE_FAIL_ALREADY_HAVE_PARENT);
             return updateResult;
