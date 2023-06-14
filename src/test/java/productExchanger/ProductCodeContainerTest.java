dea721134f84c8dc892268a36f1d47a5c4e4375e
HyeKyungAhn <hai1021@hanmail.net>
2023-06-14 15:48:14 +0900

    test: Add two test for product container

--- src/test/java/productExchanger/ProductCodeContainerTest.java
+++ src/test/java/productExchanger/ProductCodeContainerTest.java
@@ -1,4 +1,47 @@
+package productExchanger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
+
 public class ProductCodeContainerTest {
-  
+    ProductCodeContainer pcc;
+
+    @Before
+    public void setUp(){
+        pcc = new ProductCodeContainer();
+    }
+
+    //20개의 코드를 파일에서 정상적으로 불러오는가?
+    @Test
+    public void successToGenerate20ProductCodesFromFile(){
+        //given
+        //when
+        HashSet<ProductCode> codes = pcc.getCodes();
+
+        //then
+        assertEquals(20, codes.size());
+    }
+
+    //20개의 코드 중, 10개가 사용자에게 제공되는가?
+    @Test
+    public void successToProvide10ProductCodes(){
+        //given
+        //when
+        Set<ProductCode> codes = pcc.getProductCodes();
+
+        int providedProductCodeNum = 0;
+        for(ProductCode productCode : codes){
+            if(productCode.isProvided()){
+                providedProductCodeNum++;
+            }
+        }
+
+        //then
+        assertEquals(10, providedProductCodeNum);
+    }
 }
